5b7053e071b010511ee9a847430986a9432af19a
Ronaldo Rampelotti <ronaldorampelotti@users.noreply.github.com>
2015-01-30 16:40:04 -0200

    Update publica-apk.bat

--- ScriptBuild/publica-apk.bat
+++ ScriptBuild/publica-apk.bat
@@ -4,6 +4,8 @@ SET DIR_RAIZ=%cd%
 set VERSAO=%BUILD_NUMBER%
 set DESTINO=%DIR_RAIZ%\ScriptBuild\BuildDist\%VERSAO%
 
+call C:\Users\appveyor\AppData\Local\Android\android-sdk\tools\android list sdk
+
 cd %DIR_RAIZ%\TesteTTS 
 IF exist bin ( rmdir /S /Q bin )
 call C:\tools\apache-ant-1.9.4\bin\ant release -Dkey.alias.password=%android_alias_password% -Dkey.store.password=%android_store_password% -Dkey.alias=hbmdmpuxada -Dkey.store=..\\hbmdm-android.keystore
